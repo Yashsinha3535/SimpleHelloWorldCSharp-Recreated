4f7ec881b344399b6cbabbc1d74014d62c5cbb35
BluePhantomGames <ostwal50558@gmail.com>
2023-09-29 20:35:27 +0530

    Last Commit
    Weird Commit

---
+++ MainConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+namespace Program{
+    public class Program{
+        public static void Main(string[] args){
+            Console.WriteLine​("Hello World!");
+        }
+    }
+}
